a7fa7596fb69e1eb805503f8bde5e19d3d2687f9
Sasha Mile Rudan <mprinc@gmail.com>
2016-02-29 13:45:48 +0100

    Added gulp-notify Changed SASS build task to use gulp-notify

--- tools/tasks/build.css.ts
+++ tools/tasks/build.css.ts
@@ -4,12 +4,28 @@ import {APP_SRC} from '../config';
 // compiles all ts files (except tests/template ones) and type definitions,
 // replace templates in them and adds sourcemaps and copies into APP_DEST
 export = function buildCss(gulp, plugins) {
+    //the title and icon that will be used for the Grunt notifications
+    var notifyInfo = {
+    	title: 'Gulp',
+    	icon: require.resolve('gulp-notify/assets/gulp-error.png')
+    };
+
+    //error notification settings for plumber
+    var plumberErrorHandler = { errorHandler: plugins.notify.onError({
+    		title: notifyInfo.title,
+    		icon: notifyInfo.icon,
+    		message: 'Error: <%= error.message %>'
+    	})
+    };
+
+
   return function () {
     // src files are all ts files (except tests/template ones) and type definitions
     let src = [
       join(APP_SRC, '**/*.scss')
     ];
     return gulp.src(src)
+      .pipe(plugins.plumber(plumberErrorHandler))
       .pipe(plugins.sass().on('error', plugins.sass.logError))
       .pipe(gulp.dest(APP_SRC));
   };
